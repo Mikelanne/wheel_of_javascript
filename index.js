7e3750cba2898bf649ca80820ebb9c604eec640a
Mikelanne <mikelanne.wood@gmail.com>
2021-04-23 18:33:11 -0400

    formatting images to cards

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const cardContainer = document.querySelector("#character-cards")
+
 fetch('http://localhost:3000/characters')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => renderCharacters(data))
+
+    function renderCharacters(charResponse) {
+        const characters = charResponse.data
+        characters.forEach(char => {
+            const card = document.createElement("div")
+            const avatar = char.attributes.image
+            const img = document.createElement("img")
+            const header = document.createElement("h2")
+            card.className = "card"
+            card.id = char.id
+            img.src = avatar
+            img.className = "character-avatar"
+            cardContainer.appendChild(card)
+            header.innerText = `${char.attributes.name}`
+            card.append(header)
+            card.append(img)
+        })
+      
+    }
