d1e0e976d73d3a7890db26c794d97007bd54c1a0
Mikelanne <mikelanne.wood@gmail.com>
2021-04-25 18:22:00 -0400

    styling and locations fetch

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ fetch('http://localhost:3000/characters')
                 const card = event.currentTarget
                 if (card.className === "card") {
                     img.remove()
-                    header.innerText =`${char.attributes.name} 
-                    ${char.attributes.title}`
+                    header.innerHTML =`${char.attributes.name}<br>
+                    <i>${char.attributes.title}</i>`
                     const p = document.createElement("p")
-                    p.innerText = `ta'veren: ${char.attributes.ta_veren}
-                    Home: ${char.attributes.home}
-                    Abilites: ${char.attributes.abilities}`
+                    p.innerHTML = `ta'veren: <i>${char.attributes.ta_veren}</i> <br>
+                    Home: <i>${char.attributes.home}</i> <br>
+                    Abilites: <i>${char.attributes.abilities}</i>`
                     card.append(p)
                     card.className = "card-back"
                 } else {
@@ -44,6 +44,31 @@ fetch('http://localhost:3000/characters')
                     header.innerText = `${char.attributes.name}`
                     card.append(header)
                     card.append(img)
+                }
+            }
+        })
+    }
+
+
+fetch('http://localhost:3000/locations')
+    .then(response => response.json())
+    .then(data => renderLocations(data))
+
+    function renderLocations(localResponse) {
+        console.log(localResponse)
+        localResponse.forEach(place => {
+            const a = document.createElement('a')
+            const locationBar = document.querySelector(".sidenav")
+            a.setAttribute('href', `#${place.name}` )
+            a.innerHTML = `${place.name}<br><br>`
+            locationBar.append(a)
+            a.addEventListener("click", displayLocation)
+
+            function displayLocation(e) { 
+                if (a.innerHTML === `${place.name}<br><br>`) {
+                    a.innerText = `${place.name}: ruled by ${place.leader}. ${place.description}`
+                } else {
+                    a.innerHTML = `${place.name}<br><br>`
                 }
             }
         })
