1203fc2633cdc56ce499a473904af3b936970fb4
Mikelanne <mikelanne.wood@gmail.com>
2021-04-26 12:14:03 -0400

    restructing to OOJ

--- src/character.js
+++ src/character.js
@@ -1,24 +1,24 @@
-// class Character {
+class Character {
 
-//     static all = []
+    static all = []
 
-//     static container = document.querySelector("#character-cards")
+    static container = document.querySelector("#character-cards")
 
-//     constructor({id, name, ta_veren, abilities, title, home, group_id, image}){
-//         this.id = id
-//         this.name = name
-//         this.ta_veren = ta_veren
-//         this.abilities = abilities
-//         this.title = title
-//         this.home = home
-//         this.groupId = group_id
-//         this.image = image
+    constructor({id, name, ta_veren, abilities, title, home, group_id, image}){
+        this.id = id
+        this.name = name
+        this.ta_veren = ta_veren
+        this.abilities = abilities
+        this.title = title
+        this.home = home
+        this.groupId = group_id
+        this.image = image
 
-//         this.element = document.createElement("div")
-//         this.element.className = "card"
-//         this.element.id = `character-${id}`
-//         this.element.dataset.id = id
+        // this.element = document.createElement("div")
+        // this.element.className = "card"
+        // this.element.id = `character-${id}`
+        // this.element.dataset.id = id
 
-//         this.element.addEventListener("click", this.
-//     }
-// }
+        // Character.all.push(this)
+    }
+}
--- src/characterApi.js
+++ src/characterApi.js
@@ -1,13 +1,44 @@
 
-// class CharacterApi {
-//     static baseURL = 'http://localhost:3000/characters'
+class CharacterApi {
+    static baseURL = 'http://localhost:3000/characters'
 
 
-//     static getCharacters() {
-//         fetch(this.baseURL)
-//         .then(resp => resp.json())
-//         .then data => {
+    static getCharacters() {
+        fetch(this.baseURL)
+        .then(response => response.json())
+        .then(data => {
+            debugger
+            // data["data"].forEach(character => {
+            //     const char = new Character({id: character.id, ...character.attributes})
+            //     char.renderCharacters()
+            // })
+        })
+    }
 
-//         }
-//     }
-// }
+    getCharacters()
+
+
+    // static renderCharacters(char){
+    //     console.log(char)
+    //     debugger
+    //     // characters.forEach(char => {
+    //     //     const card = document.querySelector(".card")
+    //     //     const avatar = char.attributes.image
+    //     //     const img = document.createElement("img")
+    //     //     const header = document.createElement("h2")
+    //     //     const headerTwo = document.createElement("h4")
+    //     //     card.className = "card"
+    //     //     card.id = char.id
+    //     //     img.src = avatar
+    //     //     img.className = "character-avatar"
+    //     //     cardContainer.appendChild(card)
+    //     //     header.innerText = `${char.attributes.name}`
+    //     //     card.append(header)
+    //     //     card.append(img)
+    //     //     card.addEventListener("click", displayBackOfCard)
+    //     // })
+    // }
+
+    // renderCharacters()
+
+}
