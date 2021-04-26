6b86a34a61ffe70bee46e2da9f2248ca21c14e19
Mikelanne <mikelanne.wood@gmail.com>
2021-04-25 21:21:04 -0400

    starting to clean up index.js

---
+++ updateindex.js
@@ -0,0 +1,15 @@
+const baseURL = 'http://localhost:3000'
+const charactersURL = baseURL + '/characters'
+const locationsURL = baseURL + '/locations'
+const groupsURL = baseURL + '/groups'
+
+
+const cardContainer = document.querySelector("#character-cards")
+const groupContainter = document.querySelector(".group-container")
+const locationBar = document.querySelector(".sidenav")
+
+const nameInput = document.getElementById("name")
+const titleInput = document.getElementById("title")
+const abilitiesInput = document.getElementById("abilities")
+const nameInput = document.getElementById("name")
+
