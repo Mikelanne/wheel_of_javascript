0c7d0cd8373fff89539d903014329d162e7b4a77
Mikelanne <mikelanne.wood@gmail.com>
2021-04-25 21:07:26 -0400

    adding broken buttons

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ fetch('http://localhost:3000/characters')
             const avatar = char.attributes.image
             const img = document.createElement("img")
             const header = document.createElement("h2")
+            // const edit = document.createElement("button")
+            // const del = document.createElement("button")
+            const headerTwo = document.createElement("h4")
+            // edit.innerText = "Edit"
+            // del.innerText = "Delete"
             card.className = "card"
             card.id = char.id
             img.src = avatar
@@ -20,23 +25,30 @@ fetch('http://localhost:3000/characters')
             card.append(header)
             card.append(img)
             card.addEventListener("click", displayBackOfCard)
+            
 
             function displayBackOfCard(event) {
                 const card = event.currentTarget
                 if (card.className === "card") {
                     img.remove()
-                    header.innerHTML =`${char.attributes.name}<br>
-                    <i>${char.attributes.title}</i>`
+                    header.innerHTML =`${char.attributes.name}<br>`
+                    headerTwo.innerHTML = `<i>${char.attributes.title}</i>`
                     const p = document.createElement("p")
                     p.className = "card-text"
                     p.innerHTML = `ta'veren: <i>${char.attributes.ta_veren}</i> <br>
                     Home: <i>${char.attributes.home}</i> <br>
                     Abilites: <i>${char.attributes.abilities}</i>`
+                    card.append(headerTwo)
                     card.append(p)
+                    // card.append(edit)
+                    // card.append(del)
                     card.className = "card-back"
                 } else {
                     const p = document.querySelector("p")
                     p.remove()
+                    headerTwo.remove()
+                    // edit.remove()
+                    // del.remove()
                     card.className = "card"
                     card.id = char.id
                     img.src = avatar
@@ -88,5 +100,12 @@ fetch('http://localhost:3000/locations')
                 const button = document.createElement("button")
                 button.innerText = groupName
                 groupContainter.append(button)
+                // button.addEventListener("click", sortCharactersByGroup)
+
+                // function sortCharactersByGroup(e) {
+                //     const selectedGroup = e.currentTarget.innerText
+                //     console.log(e)
+                //     debugger
+                // }
             })
         }
