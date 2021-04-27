ab7609db9a5e9d5d5f18955ff340673fd0842179
Mikelanne <mikelanne.wood@gmail.com>
2021-04-27 18:54:59 -0400

    rendering back of card on click

--- src/character.js
+++ src/character.js
@@ -22,19 +22,48 @@ class Character {
         Character.all.push(this)
 
         this.renderCards()
+
+        // this.element.addEventListener("click", renderBackOfCards)
     }
 
     renderCards() {
         const avatar = this.image
         const img = document.createElement("img")
         const header = document.createElement("h2")
-        const headerTwo = document.createElement("h4")
         img.src = avatar
         img.className = "character-avatar"
         header.innerText = `${this.name}`
         this.element.append(header)
         this.element.append(img)
         cardContainer.append(this.element)
+        this.element.addEventListener("click", this.renderBackOfCards)
     }
+
+    renderBackOfCards = () => {
+        if (this.element.className === "card") {
+            this.element.className = "card-back"
+            this.element.lastElementChild.remove()
+            const headerTwo = document.createElement("h4")
+            const p = document.createElement("p")
+            headerTwo.innerHTML = `<i>${this.title}</i>`
+            p.innerHTML = `ta'veren: <i> ${this.ta_veren}</i><br> Home: <i>${this.home}</i><br> Abilities: <i>${this.abilities}</i>`
+            this.element.append(headerTwo)
+            this.element.append(p)
+        } else {
+            this.element.className = "card"
+            const h4 = this.element.querySelector("h4")
+            const p = this.element.querySelector("p")
+            h4.remove()
+            p.remove()
+            const avatar = this.image
+            const img = document.createElement("img")
+            img.src = avatar
+            img.className = "character-avatar"
+            this.element.append(img)
+        }
+
+    }
+
+
 
 }
